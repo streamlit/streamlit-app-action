b62fd7443cf5c9f960221085fd6bc35f929546ed
Joshua Carroll <joshua.carroll@snowflake.com>
2023-10-27 17:05:44 -0700

    Fix bug

--- st_smoke_test.py
+++ st_smoke_test.py
@@ -1,6 +1,11 @@
 import os
 from streamlit.testing.v1 import AppTest
 
+def test_noop():
+    # Needed to work around result xml bug
+    # See https://github.com/pmeier/pytest-results-action/issues/10#issuecomment-1677254333
+    assert True
+
 def test_smoke():
     APP_PATH = os.getenv("APP_PATH", default="streamlit_app.py")
     at = AppTest.from_file(APP_PATH).run()
