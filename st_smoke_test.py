c03e9e4e8dd5ca64e6a16b733cdfca68d9492f37
Joshua Carroll <joshua.carroll@snowflake.com>
2023-10-27 17:01:18 -0700

    Fix test

--- st_smoke_test.py
+++ st_smoke_test.py
@@ -1,7 +1,7 @@
 import os
 from streamlit.testing.v1 import AppTest
 
-def smoke_test():
+def test_smoke():
     APP_PATH = os.getenv("APP_PATH", default="streamlit_app.py")
     at = AppTest.from_file(APP_PATH).run()
     assert not at.exception
